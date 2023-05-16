9b22d2e6bb5ef45433b24df1cbf57b2b33556991
Zachary Lazzara <zachary@lazzara.ca>
2023-05-16 10:45:52 -0400

    Non-square images are now resized and padded

--- cli-tent-detector/application/utils.py
+++ cli-tent-detector/application/utils.py
@@ -62,28 +62,46 @@ class Tiler():
     def __init__(self, kernel_size: tuple[int, int]) -> None:
         self.kernel_size = kernel_size
 
-    def __tile(self, image: Image, output_dir: Path, kernel_size: tuple[int, int], output_format: str, **kwargs) -> list[Path]:
-            # Adapted from https://towardsdatascience.com/efficiently-splitting-an-image-into-tiles-in-python-using-numpy-d1bf0dd7b6f7
-            output_dir.mkdir(parents=kwargs.get('make_tiles_parents', False), exist_ok=kwargs.get('overwrite_tiles_dir', False))
+    def __tile(self, image: np.ndarray, output_dir: Path, kernel_size: tuple[int, int], output_format: str, **kwargs) -> list[Path]:
+        output_dir.mkdir(parents=kwargs.get('make_tiles_parents', False), exist_ok=kwargs.get('overwrite_tiles_dir', False))
+        image = Image.fromarray(image)
 
-            image_height, image_width, image_channels = image.shape
-            tile_height, tile_width = kernel_size
+        # Adapted from https://jdhao.github.io/2017/11/06/resize-image-to-square-with-padding/
+        if (image.size[0] % kernel_size[0] + image.size[1] % kernel_size[1]) != 0:
+            image_size = image.size[:2]
+            scale = max(image_size) // max(kernel_size)
+            target_size = tuple(x * scale for x in kernel_size)
+            aspect_ratio = float(max(target_size)) / max(image_size)
 
-            tiles = image.reshape(image_height//tile_height, tile_height,
-                                    image_width//tile_width, tile_width, image_channels).swapaxes(1, 2)
+            new_size = image_size
+            if aspect_ratio != 1.0:
+                new_size = tuple(int(x * aspect_ratio) for x in image_size)
+                image = image.resize(new_size, Image.ANTIALIAS)
 
-            image_paths = []
+            if image.size[0] != image.size[1]:
+                zero_pad = Image.new('RGB', target_size, (0, 0, 0))
+                zero_pad.paste(image, (0, 0, new_size[0], new_size[1]))
+                image = zero_pad
 
-            index = 1
-            for row in tiles:
-                for column in row:
-                    filename = output_dir / f'tile_{index:0{kwargs.get("pad_zeros", 3)}d}{output_format}'
-                    Image.fromarray(column).save(filename)
-                    image_paths.append(filename)
-                    index += 1
+        # Adapted from https://towardsdatascience.com/efficiently-splitting-an-image-into-tiles-in-python-using-numpy-d1bf0dd7b6f7
+        image = np.asarray(image)
+        image_height, image_width, image_channels = image.shape
+        tile_height, tile_width = kernel_size
 
-            return image_paths
-    
+        tiles = image.reshape(image_height // tile_height, tile_height, image_width // tile_width, tile_width, image_channels).swapaxes(1, 2)
+
+        image_paths = []
+
+        index = 1
+        for row in tiles:
+            for column in row:
+                filename = output_dir / f'tile_{index:0{kwargs.get("pad_zeros", 3)}d}{output_format}'
+                Image.fromarray(column).save(filename)
+                image_paths.append(filename)
+                index += 1
+
+        return image_paths
+
     # def _merge(self, tiles: torch.Tensor, tile_row) -> torch.Tensor:
     #     """Merges a row of tiles. The total number of tiles must be divisible by 2."""
     #     if tiles == None:
@@ -102,17 +120,19 @@ class Tiler():
         with Image.open(xy_paths['x_paths'][0]).convert("RGB") as x:
             x = np.asarray(x)
         if x.shape[:2] > self.kernel_size:
-            paths['x_paths'] = self.__tile(x, x_path.parent / 'x_tiles', self.kernel_size, x_path.suffix, **kwargs)
+            paths['x_paths'] = self.__tile(
+                x, x_path.parent / 'x_tiles', self.kernel_size, x_path.suffix, **kwargs)
             if y_path:
                 with Image.open(xy_paths['y_paths'][0]).convert("L") as y:
                     y = np.asarray(y)
-                paths['y_paths'] = self.__tile(y, y_path.parent / 'y_tiles', self.kernel_size, y_path.suffix, **kwargs)
+                paths['y_paths'] = self.__tile(
+                    y, y_path.parent / 'y_tiles', self.kernel_size, y_path.suffix, **kwargs)
             else:
                 paths['y_paths'] = [None for _ in paths['x_paths']]
             return pd.DataFrame.from_dict(paths).sort_values('x_paths', ignore_index=True)
         else:
             return None
-        
+
     # def merge(self, loader: DataLoader, output_name_x: str, output_name_y: str, pbar: tqdm = None) -> tuple[str, str]:
     #     """Tiles using a loader. Total number of tiles must be divisible by 2."""
     #     output_path_x = output_path_y = ''
@@ -137,4 +157,3 @@ class Tiler():
     #     return output_path_x, output_path_y
 
     # TODO: move merge functions to Tiler from Visualizations
-
